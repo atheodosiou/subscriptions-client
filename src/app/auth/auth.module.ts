40457e282118e91b7cc0198e6b2e1a7c109c5ab9
Anastasios THEODOSIOU <Anastasios.THEODOSIOU@arhs-dev-hellas.com>
2022-11-22 01:26:56 +0200

    Add bg image to login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthComponent } from './auth.component';
 import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -20,6 +19,6 @@ import { LoginComponent } from './login/login.component';
       },
     ]),
   ],
-  declarations: [AuthComponent, RegisterComponent, LoginComponent],
+  declarations: [RegisterComponent, LoginComponent],
 })
 export class AuthModule {}
